6bc9199f32225bf9556832b9b017b5ab51f72022
Pablo de la Peña <p@shedcollective.org>
2015-12-14 21:19:46 +0000

    Broke the CMS widgeteditor into it's own standalone component
    + Removed old nails forms JS/assets for the media managers
    + CMS Areas no longer need custom JS

--- assets/js/nails.forms.js
+++ assets/js/nails.forms.js
@@ -1,204 +1,113 @@
+/* globals NAILS_Admin_CMS_WidgetEditor, console */
 var NAILS_Forms;
 NAILS_Forms = function()
 {
-    this.__construct = function()
+    var base = this;
+
+    // --------------------------------------------------------------------------
+
+    base.__construct = function()
     {
-        this._init_form_field_mm();
-        this._init_form_field_mm_image();
-        this._init_form_field_multiimage();
+        //  @todo: move into the CMS module
+        base.initWidgetEditors();
     };
 
     // --------------------------------------------------------------------------
 
-    this._init_form_field_mm = function()
+    base.widgetEditor = null;
+    base.activeArea = null;
+    base.initWidgetEditors = function()
     {
-        //  Bind to the chooser, open the Fancybox
-        $(document).on('click', '.field.mm-file a.mm-file-choose', function()
-        {
-            var _href,_w,_h;
+        //  Set up an instance of the widget editor
+        base.widgetEditor = new NAILS_Admin_CMS_WidgetEditor();
 
-            _href = $(this).attr('href');
-            _href += _href.indexOf('?') >= 0 ? '&isModal=1' : '?isModal=1';
+        //  Populate the editor with existing areas
+        $('.field.cms-widgets .open-editor').each(function() {
 
-            if ($.fancybox.isOpen && $.fancybox.opts.type !== 'iframe')
-            {
-                _href += '&reopenModal=' + encodeURIComponent($.fancybox.opts.href);
-                $.fancybox.close();
+            //  Look for the associated input
+            var key   = $(this).data('key');
+            var input = $(this).siblings('input.widget-data');
+
+            if (input.length) {
+
+                try {
+
+                    var widgetData = JSON.parse(input.val());
+                    base.widgetEditor.setAreaData(key, widgetData);
+
+                } catch (e) {}
             }
+        });
 
-            _w = $(this).data('width') ? $(this).data('width') : null;
-            _h = $(this).data('height') ? $(this).data('height') : null;
-
-            $.fancybox.open({
-                href: _href,
-                type: 'iframe',
-                width: _w,
-                height: _h,
-                iframe: {
-                    preload: false // fixes issue with iframe and IE
-                }
-            });
-
+        //  Bind to the thigns
+        $(document).on('click', '.field.cms-widgets .open-editor', function() {
+            base.activeArea = $(this);
+            base.log('Opening Editor for area: ' + base.activeArea.data('key'));
+            base.widgetEditor.show(base.activeArea.data('key'));
             return false;
         });
 
-        //  Bind to remover
-        $(document).on('click', '.field.mm-file a.mm-file-remove', function()
+        $(base.widgetEditor).on('widgeteditor-close', function()
         {
-            $(this).closest('.field.mm-file').find('.mm-file-preview').html('');
-            $(this).closest('.field.mm-file').find('.mm-file-value').val('');
-            $(this).closest('.field.mm-file').find('.mm-file-remove').css('display', 'none');
+            base.log('Editor Closing, getting area data and saving to input');
+            var data  = base.widgetEditor.getAreaData(base.activeArea.data('key'));
+            var input = base.activeArea.siblings('input.widget-data');
 
-            return false;
-        });
-    };
-
-    // --------------------------------------------------------------------------
-
-    this._callback_form_field_mm = function(bucket, file, id, reopen, fieldId)
-    {
-        if (file.length === 0)
-        {
-            $('#' + fieldId + ' .mm-file-preview').html('');
-            $('#' + fieldId + ' .mm-file-value').val('');
-            $('#' + fieldId + ' .mm-file-remove').css('display', 'none');
-
-            // --------------------------------------------------------------------------
-
-            //  Reopen facybox?
-            if (reopen.length)
-            {
-                $.fancybox.open({
-                    href:reopen
-                });
+            if (input.length) {
+                input.val(JSON.stringify(data));
             }
 
-            return;
-        }
-
-        // --------------------------------------------------------------------------
-
-        var _scheme = $('#' + fieldId).data('scheme');
-        var _file   = file.split('.');
-
-        _scheme = _scheme.replace('{[bucket]}', bucket);
-        _scheme = _scheme.replace('{[filename]}', _file[0]);
-        _scheme = _scheme.replace('{[extension]}', '.' + _file[1]);
-
-        $('#' + fieldId + ' .mm-file-preview').html('<a href="' + _scheme + '">Download</a>');
-        $('#' + fieldId + ' .mm-file-value').val(id);
-        $('#' + fieldId + ' .mm-file-remove').css('display', 'inline-block');
-
-        // --------------------------------------------------------------------------
-
-        //  Reopen facybox?
-        if (reopen.length)
-        {
-            $.fancybox.open({
-                href:reopen
-            });
-        }
-    };
-
-    // --------------------------------------------------------------------------
-
-    this._init_form_field_mm_image = function()
-    {
-        //  Bind to the chooser, open the Fancybox
-        $(document).on('click', '.field.mm-image a.mm-image-choose', function()
-        {
-            var _href,_w,_h;
-
-            _href = $(this).attr('href');
-            _href += _href.indexOf('?') >= 0 ? '&isModal=1' : '?isModal=1';
-
-            if ($.fancybox.isOpen && $.fancybox.opts.type !== 'iframe')
-            {
-                _href += '&reopenModal=' + encodeURIComponent($.fancybox.opts.href);
-                $.fancybox.close();
-            }
-
-            _w = $(this).data('width') ? $(this).data('width') : null;
-            _h = $(this).data('height') ? $(this).data('height') : null;
-
-            $.fancybox.open({
-                href: _href,
-                type: 'iframe',
-                width: _w,
-                height: _h,
-                iframe: {
-                    preload: false // fixes issue with iframe and IE
-                }
-            });
-
-            return false;
-        });
-
-        //  Bind to remover
-        $(document).on('click', '.field.mm-image a.mm-image-remove', function()
-        {
-            $(this).closest('.field.mm-image').find('.mm-image-preview').html('');
-            $(this).closest('.field.mm-image').find('.mm-image-value').val('');
-            $(this).closest('.field.mm-image').find('.mm-image-remove').css('display', 'none');
-
-            return false;
+            base.activeArea = null;
         });
     };
 
     // --------------------------------------------------------------------------
 
-    this._callback_form_field_mm_image = function(bucket, file, id, reopen, fieldId)
+    /**
+     * Write a log to the console
+     * @param  {String} message The message to log
+     * @param  {Mixed}  payload Any additional data to display in the console
+     * @return {Void}
+     */
+    base.log = function(message, payload)
     {
-        if (file.length === 0)
-        {
-            $('#' + fieldId + ' .mm-image-preview').html('');
-            $('#' + fieldId + ' .mm-image-value').val('');
-            $('#' + fieldId + ' .mm-image-remove').css('display', 'none');
+        if (typeof(console.log) === 'function') {
 
-            // --------------------------------------------------------------------------
+            if (payload !== undefined) {
 
-            //  Reopen facybox?
-            if (reopen.length)
-            {
-                $.fancybox.open({
-                    href:reopen
-                });
+                console.log('Nails Forms:', message, payload);
+
+            } else {
+
+                console.log('Nails Forms:', message);
             }
-
-            return;
-        }
-
-        // --------------------------------------------------------------------------
-
-        var _scheme = $('#' + fieldId).data('scheme');
-        var _file   = file.split('.');
-
-        _scheme = _scheme.replace('{[bucket]}', bucket);
-        _scheme = _scheme.replace('{[filename]}', _file[0]);
-        _scheme = _scheme.replace('{[extension]}', '.' + _file[1]);
-
-        $('#' + fieldId + ' .mm-image-preview').html('<img src="' + _scheme + '" / >');
-        $('#' + fieldId + ' .mm-image-value').val(id);
-        $('#' + fieldId + ' .mm-image-remove').css('display', 'inline-block');
-
-        // --------------------------------------------------------------------------
-
-        //  Reopen facybox?
-        if (reopen.length)
-        {
-            $.fancybox.open({
-                href:reopen
-            });
         }
     };
 
     // --------------------------------------------------------------------------
 
-    this._init_form_field_multiimage = function()
+    /**
+     * Write a warning to the console
+     * @param  {String} message The message to warn
+     * @param  {Mixed}  payload Any additional data to display in the console
+     * @return {Void}
+     */
+    base.warn = function(message, payload)
     {
+        if (typeof(console.warn) === 'function') {
+
+            if (payload !== undefined) {
+
+                console.warn('Nails Forms:', message, payload);
+
+            } else {
+
+                console.warn('Nails Forms:', message);
+            }
+        }
     };
 
     // --------------------------------------------------------------------------
 
-    return this.__construct();
+    return base.__construct();
 };
