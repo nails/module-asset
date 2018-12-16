5f1bfad45eeb691043994ef0674e23d9e8daea6d
Pablo de la Peña <p@shedcollective.org>
2018-12-16 18:58:56 +0000

    Updated jQuery to version 3

--- assets/bower_components/jquery/src/jquery.js
+++ assets/bower_components/jquery/src/jquery.js
@@ -1,17 +1,17 @@
-define([
+define( [
 	"./core",
 	"./selector",
 	"./traversing",
 	"./callbacks",
 	"./deferred",
+	"./deferred/exceptionHook",
 	"./core/ready",
-	"./support",
 	"./data",
 	"./queue",
 	"./queue/delay",
 	"./attributes",
 	"./event",
-	"./event/alias",
+	"./event/focusin",
 	"./manipulation",
 	"./manipulation/_evalUrl",
 	"./wrap",
@@ -23,6 +23,7 @@ define([
 	"./ajax/script",
 	"./ajax/jsonp",
 	"./ajax/load",
+	"./event/ajax",
 	"./effects",
 	"./effects/animatedSelector",
 	"./offset",
@@ -32,6 +33,8 @@ define([
 	"./exports/global"
 ], function( jQuery ) {
 
+"use strict";
+
 return jQuery;
 
-});
+} );
