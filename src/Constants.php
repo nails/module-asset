470233f55ac855e38d4fadad7977da122d5ff84a
Pablo de la Peña <p@shedcollective.org>
2021-03-26 15:53:50 +0000

    Adds Module constants class

---
+++ src/Constants.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nails\Asset;
+
+/**
+ * Class Constants
+ *
+ * @package Nails\Asset
+ */
+class Constants
+{
+    /**
+     * The slug for this module
+     *
+     * @var string
+     */
+    const MODULE_SLUG = 'nails/module-asset';
+}
